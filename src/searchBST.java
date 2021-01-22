a2c92ef7f104387319db44aaf5a8268381fbd05f
Csaintilienc1 <saintilienclaudel@gmail.com>
2021-01-21 21:02:14 -0500

    Initial commit

---
+++ src/searchBST.java
@@ -0,0 +1,19 @@
+
+public class searchBST{
+
+
+    class node {
+        int val;
+        node right;
+        node left;
+        node(int val) {
+            this.val = val;
+        }
+    }
+
+
+
+
+}
+
+
